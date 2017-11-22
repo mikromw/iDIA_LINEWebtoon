7e271a4035e06165d4509f510aa3cfff1705cadd
misasan1612@gmail.com <misasan1612@gmail.com>
2017-11-23 00:12:21 +0700

    splash screen trying

---
+++ app/src/main/java/com/example/user/pmdproject/SplashScreen.java
@@ -0,0 +1,51 @@
+package com.example.user.pmdproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        startHeavyProcessing();
+
+    }
+
+    private void startHeavyProcessing(){
+        new LongOperation().execute("");
+    }
+
+    private class LongOperation extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            //some heavy processing resulting in a Data String
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.interrupted();
+                }
+            }
+            return "Loaded";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent i = new Intent(SplashScreen.this, MainActivity.class);
+            i.putExtra("data", result);
+            startActivity(i);
+            finish();
+        }
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }
+}
