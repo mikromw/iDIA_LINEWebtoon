74da41afbe900edbb79263008180d4b47c44da58
misasan1612@gmail.com <misasan1612@gmail.com>
2017-12-05 14:28:34 +0700

    login activity added

---
+++ app/src/main/java/com/example/user/pmdproject/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.user.pmdproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    private LinearLayout txtSkip;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initView();
+        initEvent();
+    }
+
+    private void initView() {
+        txtSkip = (LinearLayout) findViewById(R.id.login_skip);
+    }
+
+    private void initEvent() {
+        txtSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
